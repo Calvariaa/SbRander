e885628b3d5ac416170a10ab68d03a6b3f4d0fb2
申致尧 <1764203060@qq.com>
2020-06-26 17:08:00 +0800

    重新生成sb.db

--- test/RandSb.py
+++ test/RandSb.py
@@ -10,9 +10,9 @@ try :
     cursor = c.execute("SELECT content FROM sba");
     count = cursor.fetchall()
     num = len(count)
-#    print(num) #输出总项数
-    randnum = random.randint(1,num) #随机一个sb
-#    print(randnum) #输出随机到的项数
+    print(num) #输出总项数
+    randnum = random.randint(1,num) - 1 #随机一个sb
+    print(randnum) #输出随机到的项数
     print(count[randnum][0]) #输出sb
 except :
     print('没这一项啊')
